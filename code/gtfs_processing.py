c30f2273429d1bd067f54c0268f4900207998a8a
Atila Jr <amsj.master@gmail.com>
2017-09-29 18:40:02 -0300

    plot shape.txt

--- code/gtfs_processing.py
+++ code/gtfs_processing.py
@@ -11,14 +11,17 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point, LineString
 
+import matplotlib.pyplot as plt
+
 gtfs_zip_folder = argv[1]
+result_path = argv[2]
 
 def read_file_in_zip(gtfs_zip_folder, file_name):
     zip_file = zipfile.ZipFile(gtfs_zip_folder)
     df_csv = pd.read_csv(zip_file.open(file_name))
     return df_csv
 
-def df_to_line_gdf(df_shapes):
+def format_shape_lines(df_shapes):
     # Zip the coordinates into a point object and convert to a GeoDataFrame
     geometry = [Point(xy) for xy in zip(df_shapes['shape_pt_lon'], df_shapes['shape_pt_lat'])]
     gdf_shapes = gpd.GeoDataFrame(df_shapes, geometry=geometry)
@@ -29,10 +32,18 @@ def df_to_line_gdf(df_shapes):
 
     return gdf_shapes
 
+def plot_gdf(gdf, plot_name):
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    gdf.plot(ax=ax)
+
+    fig.savefig(plot_name)
+
 df_shapes = read_file_in_zip(gtfs_zip_folder, 'shapes.txt')
 df_stops = read_file_in_zip(gtfs_zip_folder, 'stops.txt')
 # print df_shapes
 # print df_stops
 
-gdf_lines = df_to_line_gdf(df_shapes)
+gdf_lines = format_shape_lines(df_shapes)
 print gdf_lines
+plot_gdf(gdf_lines, result_path + 'shapes.pdf')
