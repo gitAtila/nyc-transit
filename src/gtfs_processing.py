2dcabcdae6f92e4143301a39e044ba804af2fe52
Atila Jr <amsj.master@gmail.com>
2017-10-17 21:00:40 -0200

    add trunk to line shape

--- src/gtfs_processing.py
+++ src/gtfs_processing.py
@@ -49,21 +49,21 @@ def split_shape_id(gdf_shapes, separator):
     return gdf_shapes
 
 def insert_trunk(gdf_lines, dict_trunk_names):
-    dict_lines = gdf_lines.to_dict()
-    dict_trunk_lines = dict()
-    for index, line in gdf_lines.iteritems():
+    list_trunks = []
+    #dict_trunk_lines = dict()
+    for index, line in gdf_lines.iterrows():
         try:
             trunk_name =  dict_trunk_names[line['shape_id'].split('.')[0]]
-            
         except KeyError:
             trunk_name = 'S'
         finally:
-            dict_trunk_lines.setdefault(trunk_name, []).append(line)
+            list_trunks.append(trunk_name)
+            #dict_trunk_lines.setdefault(trunk_name, []).append(line)
+    gdf_lines['trunk'] = pd.Series(list_trunks)
+    # for trunk, list_lines in dict_trunk_lines.iteritems():
+    #     dict_trunk_lines[trunk] = gpd.GeoDataFrame(list_lines)
 
-    for trunk, list_lines in dict_trunk_lines.iteritems():
-        dict_trunk_lines[trunk] = gpd.GeoDataFrame(list_lines)
-
-    return dict_trunk_lines
+    return gdf_lines
 
 def plot_gdf(gdf, plot_name):
     fig, ax = plt.subplots()
@@ -127,15 +127,16 @@ def path_between_stops(stop_1, stop_2, gdf_shape_line):
     Read plot shape.txt
 '''
 
-df_shapes = read_file_in_zip(gtfs_zip_folder, 'shapes.txt')
-gdf_lines = format_shape_lines(df_shapes)
-gdf_lines = split_shape_id(gdf_lines, '.')
-print gdf_lines
-gdf_lines.to_file(result_path+'gtfs_lines.shp')
-
 nyc_trunk_names = {'1':'1', '2':'1', '3':'1', '4':'4', '5':'4', '6':'4', '7':'7',\
  'A':'A', 'C':'A', 'E':'A', 'B':'B', 'D':'B', 'F':'B', 'M':'B', 'G':'G', 'J':'J',\
  'Z':'J', 'L':'L', 'N':'N','Q':'N','R':'N', 'W':'N', 'S':'S', 'SI':'SI'}
+
+df_shapes = read_file_in_zip(gtfs_zip_folder, 'shapes.txt')
+gdf_lines = format_shape_lines(df_shapes)
+gdf_lines = insert_trunk(gdf_lines, nyc_trunk_names)
+gdf_lines = split_shape_id(gdf_lines, '.')
+print gdf_lines
+gdf_lines.to_file(result_path+'gtfs_lines.shp')
 
 nyc_line_colors = {'1':'#ee352e', '4':'#00933c', '7':'#b933ad', 'A':'#2850ad',\
  'B':'#ff6319', 'G':'#6cbe45', 'J':'#996633', 'L':'#a7a9ac', 'N':'#fccc0a',\
