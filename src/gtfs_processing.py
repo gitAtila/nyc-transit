88cbb626cad2cae0402ae5a81c528aa864cec736
Atila Jr <amsj.master@gmail.com>
2017-10-17 19:47:28 -0200

    split shape_id into id and line

--- src/gtfs_processing.py
+++ src/gtfs_processing.py
@@ -25,6 +25,8 @@ def format_points(df_points, lon_column, lat_column):
     # Zip the coordinates into a point object and convert to a GeoDataFrame
     geometry = [Point(xy) for xy in zip(df_points[lon_column], df_points[lat_column])]
     gdf_points = gpd.GeoDataFrame(df_points, geometry=geometry)
+    del gdf_points[lon_column]
+    del gdf_points[lat_column]
 
     return gdf_points
 
@@ -37,6 +39,31 @@ def format_shape_lines(df_shapes):
     gdf_shapes = gpd.GeoDataFrame(gdf_shapes, geometry='geometry')
 
     return gdf_shapes
+
+def split_shape_id(gdf_shapes, separator):
+    s = gdf_shapes['shape_id'].apply(lambda x:x.split(separator))
+    gdf_shapes['id'] = s.apply(lambda x: x[-1])
+    gdf_shapes['line'] = s.apply(lambda x: x[0])
+    del gdf_shapes['shape_id']
+
+    return gdf_shapes
+
+def insert_trunk(gdf_lines, dict_trunk_names):
+    dict_lines = gdf_lines.to_dict()
+    dict_trunk_lines = dict()
+    for index, line in gdf_lines.iteritems():
+        try:
+            trunk_name =  dict_trunk_names[line['shape_id'].split('.')[0]]
+            
+        except KeyError:
+            trunk_name = 'S'
+        finally:
+            dict_trunk_lines.setdefault(trunk_name, []).append(line)
+
+    for trunk, list_lines in dict_trunk_lines.iteritems():
+        dict_trunk_lines[trunk] = gpd.GeoDataFrame(list_lines)
+
+    return dict_trunk_lines
 
 def plot_gdf(gdf, plot_name):
     fig, ax = plt.subplots()
@@ -101,8 +128,10 @@ def path_between_stops(stop_1, stop_2, gdf_shape_line):
 '''
 
 df_shapes = read_file_in_zip(gtfs_zip_folder, 'shapes.txt')
-
 gdf_lines = format_shape_lines(df_shapes)
+gdf_lines = split_shape_id(gdf_lines, '.')
+print gdf_lines
+gdf_lines.to_file(result_path+'gtfs_lines.shp')
 
 nyc_trunk_names = {'1':'1', '2':'1', '3':'1', '4':'4', '5':'4', '6':'4', '7':'7',\
  'A':'A', 'C':'A', 'E':'A', 'B':'B', 'D':'B', 'F':'B', 'M':'B', 'G':'G', 'J':'J',\
@@ -114,7 +143,7 @@ nyc_line_colors = {'1':'#ee352e', '4':'#00933c', '7':'#b933ad', 'A':'#2850ad',\
 
 
 #plot_gdf(gdf_lines, result_path + 'shapes.pdf')
-dict_trunk_lines = group_line_by_trunk(gdf_lines, nyc_trunk_names)
+#dict_trunk_lines = group_line_by_trunk(gdf_lines, nyc_trunk_names)
 #plot_shape_trunk(dict_trunk_lines, nyc_line_colors, result_path + 'shapes_color.pdf')
 
 '''
@@ -124,6 +153,7 @@ dict_trunk_lines = group_line_by_trunk(gdf_lines, nyc_trunk_names)
 df_stops = read_file_in_zip(gtfs_zip_folder, 'stops.txt')
 gdf_stops = format_points(df_stops, 'stop_lon', 'stop_lat')
 print gdf_stops
+gdf_stops.to_file(result_path+'gtfs_stops.shp')
 # plot_gdf(gdf_stops, result_path + 'stops.pdf')
 
 '''
