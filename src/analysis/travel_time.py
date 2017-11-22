27e900f74419d2703515690b2e5568ff83a454c8
Atila Jr <amsj.master@gmail.com>
2017-11-22 15:36:49 -0200

    analyse sbway passenger travel time

--- src/analysis/travel_time.py
+++ src/analysis/travel_time.py
@@ -1,13 +1,19 @@
 '''
-    Analyse the travel time of taxisharing routes
+    Analyse travel time of taxisharing routes
 '''
 from sys import argv
 import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from statsmodels.distributions.empirical_distribution import ECDF
 
 sbwy_individual_trip_path = argv[1]
 taxi_individual_trip_path = argv[2]
 sbwy_taxi_maching_path = argv[3]
 taxisharing_trip_path = argv[4]
+chart_name = argv[5]
 
 def group_df_rows(df, key_label):
     dict_grouped = dict()
@@ -17,50 +23,130 @@ def group_df_rows(df, key_label):
         dict_grouped.setdefault(key, []).append(row.to_dict())
     return dict_grouped
 
+def sbwy_duration(df_sbwy_individual_trip, df_taxi_individual_trip, dict_matching, df_taxisharing_trip):
+
+    list_sbwy_duration = []
+    for match_id, match_route in dict_matching.iteritems():
+        #print match_id
+        # select subway and taxi matching trips
+        sbwy_matching_trip = [position for position in match_route if position['sequence'] < 7]
+        taxi_matching_trip = [position for position in match_route if position['sequence'] >= 7]
+
+        # sort sequence
+        sbwy_matching_trip = sorted(sbwy_matching_trip, key=lambda position:position['sequence'])
+        taxi_matching_trip = sorted(taxi_matching_trip, key=lambda position:position['sequence'])
+
+        sbwy_trip_id = sbwy_matching_trip[0]['sampn_perno_tripno']
+        taxi_trip_id = taxi_matching_trip[0]['sampn_perno_tripno']
+        #print 'sbwy_trip_id', sbwy_trip_id
+        #print 'taxi_trip_id', taxi_trip_id
+
+        # taxi passenger individual route time
+        # taxi_individual_trip = df_taxi_individual_trip[df_taxi_individual_trip['sampn_perno_tripno'] == taxi_trip_id]
+        # taxi_individual_trip = taxi_individual_trip.sort_values(by='duration')
+
+        # subway passenger individual route time
+        sbwy_individual_trip = df_sbwy_individual_trip[df_sbwy_individual_trip['sampn_perno_tripno'] == sbwy_trip_id]
+        sbwy_individual_trip = sbwy_individual_trip.sort_values(by='date_time')
+        sbwy_origin_time = sbwy_individual_trip['date_time'].iloc[0]
+        sbwy_individual_destination_time = sbwy_individual_trip['date_time'].iloc[-1]
+
+        #print 'sbwy_passenger_origin', sbwy_origin_time
+        #print 'sbwy_passenger_individual_destination', sbwy_individual_destination_time
+        duration_individual = (sbwy_individual_destination_time - sbwy_origin_time).total_seconds()
+
+        # subway passenger time with taxisharing
+        taxisharing_route = df_taxisharing_trip[(df_taxisharing_trip['sbwy_sampn_perno_tripno'] == sbwy_trip_id)\
+        & (df_taxisharing_trip['taxi_sampn_perno_tripno'] == taxi_trip_id)].iloc[0]
+
+        sbwy_taxisharing_destination_time = taxisharing_route['last_destination_time']
+        taxi_taxisharing_destination_time = taxisharing_route['first_destination_time']
+        if taxisharing_route['sbwy_destination_first']:
+            sbwy_taxisharing_destination_time = taxisharing_route['first_destination_time']
+            taxi_taxisharing_destination_time = taxisharing_route['last_destination_time']
+        duration_taxisharing = (sbwy_taxisharing_destination_time - sbwy_origin_time).total_seconds()
+
+        #print 'sbwy_passenger_taxisharing_destination', sbwy_taxisharing_destination_time
+        dict_sbwy_durantion = {'sbwy_trip_id': sbwy_trip_id, 'taxi_trip_id': taxi_trip_id,\
+        'origin_date_time': sbwy_origin_time, 'duration_individual': duration_individual,\
+        'duration_taxisharing': duration_taxisharing}
+        list_sbwy_duration.append(dict_sbwy_durantion)
+        #print dict_sbwy_durantion
+        #break
+
+    df_sbwy_duration = pd.DataFrame(list_sbwy_duration)
+    df_sbwy_duration = df_sbwy_duration[['sbwy_trip_id', 'taxi_trip_id', 'origin_date_time',\
+    'duration_individual', 'duration_taxisharing']]
+
+    return df_sbwy_duration
+
 df_sbwy_individual_trip = pd.read_csv(sbwy_individual_trip_path)
 df_taxi_individual_trip = pd.read_csv(taxi_individual_trip_path)
-print df_sbwy_individual_trip
+
+df_sbwy_individual_trip['date_time'] = pd.to_datetime(df_sbwy_individual_trip['date_time'])
 
 df_sbwy_taxi_matching = pd.read_csv(sbwy_taxi_maching_path)
 dict_matching = group_df_rows(df_sbwy_taxi_matching, 'match_id')
 
 df_taxisharing_trip = pd.read_csv(taxisharing_trip_path)
+df_taxisharing_trip['first_destination_time'] = pd.to_datetime(df_taxisharing_trip['first_destination_time'])
+df_taxisharing_trip['last_destination_time'] = pd.to_datetime(df_taxisharing_trip['last_destination_time'])
 
-for match_id, match_route in dict_matching.iteritems():
-    print match_id
-    # select subway and taxi matching trips
-    sbwy_matching_trip = [position for position in match_route if position['sequence'] < 7]
-    taxi_matching_trip = [position for position in match_route if position['sequence'] >= 7]
-    # sort sequence
-    sbwy_matching_trip = sorted(sbwy_matching_trip, key=lambda position:position['sequence'])
-    taxi_matching_trip = sorted(taxi_matching_trip, key=lambda position:position['sequence'])
+# distinct sbwy travels
+print 'unique individual sbwy', len(df_sbwy_individual_trip['sampn_perno_tripno'].unique())
+print 'unique individual taxi', len(df_taxi_individual_trip['sampn_perno_tripno'].unique())
 
-    sbwy_trip_id = sbwy_matching_trip[0]['sampn_perno_tripno']
+df_sbwy_duration = sbwy_duration(df_sbwy_individual_trip, df_taxi_individual_trip, dict_matching, df_taxisharing_trip)
+#print df_sbwy_duration
 
-    sbwy_individual_trip = df_sbwy_individual_trip[df_sbwy_individual_trip['sampn_perno_tripno'] == sbwy_trip_id]
-    #sbwy_individual_origin_time = sbwy_individual_trip['']
+count_good_taxisharing = 0
+list_time_saving = []
+list_good_duration_individual = []
+list_good_duration_taxisharing = []
+for index, durations in df_sbwy_duration.iterrows():
+    if durations['duration_taxisharing'] < durations['duration_individual']:
+        count_good_taxisharing += 1
+        time_saving = (durations['duration_individual'] - durations['duration_taxisharing'])/60
+        list_time_saving.append(time_saving)
 
-    # subway passenger time with taxisharing
-    print 'sbwy_trip_id', sbwy_trip_id
-    sbwy_taxisharing_origin_time = sbwy_matching_trip[0]['date_time']
-    print sbwy_taxisharing_origin_time
-    df_taxisharing_sbwy = df_taxisharing_trip[df_taxisharing_trip['sbwy_sampn_perno_tripno'] == sbwy_trip_id]
-    for index, taxisharing_sbwy in df_taxisharing_sbwy.iterrows():
-        sbwy_taxisharing_destination_time = taxisharing_sbwy['last_destination_time']
-        if taxisharing_sbwy['sbwy_destination_first']:
-            sbwy_taxisharing_destination_time = taxisharing_sbwy['first_destination_time']
+        list_good_duration_taxisharing.append(durations['duration_taxisharing'])
+        list_good_duration_individual.append(durations['duration_individual'])
 
-        print '\t', sbwy_taxisharing_destination_time
+print len(df_sbwy_duration)
+print count_good_taxisharing
+print float(count_good_taxisharing)/len(df_sbwy_duration)
+print 'statistics'
+print 'mean', np.mean(list_time_saving)
+print 'std', np.std(list_time_saving)
+print 'var', np.var(list_time_saving)
+print 'min', np.min(list_time_saving)
+print 'max', np.max(list_time_saving)
 
-    # taxi passenger time with taxisharing
-    taxi_trip_id = taxi_matching_trip[0]['sampn_perno_tripno']
-    print 'taxi_trip_id', taxi_trip_id
-    taxi_taxisharing_origin_time = taxi_matching_trip[0]['date_time']
-    print taxi_taxisharing_origin_time
-    df_taxisharing_taxi = df_taxisharing_trip[df_taxisharing_trip['taxi_sampn_perno_tripno'] == taxi_trip_id]
-    for index, taxisharing_taxi in df_taxisharing_taxi.iterrows():
-        taxi_taxisharing_destination_time = taxisharing_taxi['first_destination_time']
-        if taxisharing_taxi['sbwy_destination_first']:
-            taxi_taxisharing_destination_time = taxisharing_taxi['last_destination_time']
-        print '\t', taxi_taxisharing_destination_time
-    break
+# plot cdf
+# list_sbwy_individual_route_duration = df_sbwy_duration['duration_individual'].tolist()
+# list_sbwy_taxisharing_route_duration = df_sbwy_duration['duration_taxisharing'].tolist()
+
+list_sbwy_individual_route_duration = list_good_duration_individual
+list_sbwy_taxisharing_route_duration = list_good_duration_taxisharing
+
+list_sbwy_individual_route_duration = [duration/60 for duration in list_sbwy_individual_route_duration]
+list_sbwy_taxisharing_route_duration = [duration/60 for duration in list_sbwy_taxisharing_route_duration]
+
+list_sbwy_taxisharing_route_duration.sort()
+ecdf_taxisharing_duration = ECDF(list_sbwy_taxisharing_route_duration)
+
+list_sbwy_individual_route_duration.sort()
+ecdf_individual_duration = ECDF(list_sbwy_individual_route_duration)
+
+fig, ax = plt.subplots()
+plt.plot(ecdf_taxisharing_duration.x, ecdf_taxisharing_duration.y, label='taxisharing duration')
+plt.plot(ecdf_individual_duration.x, ecdf_individual_duration.y, label='individual duration')
+
+#ax.xaxis.set_major_locator(ticker.MultipleLocator(60)) # set x sticks interal
+plt.grid()
+plt.legend()
+ax.set_title('Subway Trips on Sunday')
+ax.set_xlabel('Travel Duration in Minutes')
+ax.set_ylabel('ECDF')
+plt.tight_layout()
+fig.savefig(chart_name)
