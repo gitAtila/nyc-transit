0a2a3f6d13573c664bbec8f3ea2182d2137c5f3e
Atila Jr <amsj.master@gmail.com>
2017-11-25 12:14:07 -0200

    computing subway and walking distances

--- src/analysis/travel_time.py
+++ src/analysis/travel_time.py
@@ -4,6 +4,7 @@
 from sys import argv
 import pandas as pd
 import numpy as np
+import math
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -13,8 +14,8 @@ sbwy_individual_trip_path = argv[1]
 taxi_individual_trip_path = argv[2]
 sbwy_taxi_maching_path = argv[3]
 taxisharing_trip_path = argv[4]
-chart_name = argv[5]
-title_name = argv[6]
+# chart_name = argv[5]
+# title_name = argv[6]
 
 def group_df_rows(df, key_label):
     dict_grouped = dict()
@@ -23,6 +24,38 @@ def group_df_rows(df, key_label):
         del row[key_label]
         dict_grouped.setdefault(key, []).append(row.to_dict())
     return dict_grouped
+
+def walking_subway_distance_duration(df_sbwy_integration_destination):
+    df_walking_positions = df_sbwy_integration_destination[df_sbwy_integration_destination['stop_id'].isnull()]
+    df_sbwy_positions = df_sbwy_integration_destination[df_sbwy_integration_destination['stop_id'].isnull() == False]
+    # print ''
+    # print df_walking_positions
+    list_walking_distances = []
+    first_distance = df_walking_positions.iloc[0]['distance']
+    previous_distance = first_distance
+
+    for index, current in df_walking_positions.iloc[1:].iterrows():
+        if current['distance'] == 0:
+            total_distance = previous_distance - first_distance
+            list_walking_distances.append(total_distance)
+            first_distance = 0
+        previous_distance = current['distance']
+    total_distance = previous_distance - first_distance
+    list_walking_distances.append(total_distance)
+
+    if len(list_walking_distances) == 0:
+        list_walking_distances.append(0.0)
+        list_walking_distances.append(0.0)
+    elif len(list_walking_distances) == 1:
+        list_walking_distances = [0.0] + list_walking_distances
+
+    # print list_walking_distances
+    #
+    # print ''
+    # print df_sbwy_positions
+    sbwy_distance = df_sbwy_positions.iloc[-1]['distance'] - df_sbwy_positions.iloc[0]['distance']
+
+    return {'walking_distance': list_walking_distances, 'sbwy_distance':sbwy_distance}
 
 def sbwy_duration(df_sbwy_individual_trip, df_taxi_individual_trip, dict_matching, df_taxisharing_trip):
 
@@ -52,9 +85,25 @@ def sbwy_duration(df_sbwy_individual_trip, df_taxi_individual_trip, dict_matchin
         sbwy_origin_time = sbwy_individual_trip['date_time'].iloc[0]
         sbwy_individual_destination_time = sbwy_individual_trip['date_time'].iloc[-1]
 
+        # find the integration position and time
+        integration_position = 0
+        for position in range(len(sbwy_individual_trip)):
+            if sbwy_individual_trip.iloc[position]['date_time'] == sbwy_matching_trip[-1]['date_time']:
+                integration_position = position
+                break
+
+        sbwy_walking_trip = sbwy_individual_trip.iloc[integration_position:]
+        print sbwy_walking_trip
+
+        sbwy_integration_position = sbwy_individual_trip[sbwy_individual_trip['date_time'] == sbwy_matching_trip[-1]['date_time']]
+        sbwy_integration_time = sbwy_integration_position['date_time'].iloc[0]
+
+        # compute walking subway time
+        dict_walking_sbwy_distances = walking_subway_distance_duration(sbwy_walking_trip)
+
         #print 'sbwy_passenger_origin', sbwy_origin_time
         #print 'sbwy_passenger_individual_destination', sbwy_individual_destination_time
-        duration_individual = (sbwy_individual_destination_time - sbwy_origin_time).total_seconds()
+        duration_individual = (sbwy_individual_destination_time - sbwy_integration_time).total_seconds()
 
         # subway passenger time with taxisharing
         taxisharing_route = df_taxisharing_trip[(df_taxisharing_trip['sbwy_sampn_perno_tripno'] == sbwy_trip_id)\
@@ -65,7 +114,7 @@ def sbwy_duration(df_sbwy_individual_trip, df_taxi_individual_trip, dict_matchin
         if taxisharing_route['sbwy_destination_first']:
             sbwy_taxisharing_destination_time = taxisharing_route['first_destination_time']
             taxi_taxisharing_destination_time = taxisharing_route['last_destination_time']
-        duration_taxisharing = (sbwy_taxisharing_destination_time - sbwy_origin_time).total_seconds()
+        duration_taxisharing = (sbwy_taxisharing_destination_time - sbwy_integration_time).total_seconds()
 
         #print 'sbwy_passenger_taxisharing_destination', sbwy_taxisharing_destination_time
         dict_sbwy_durantion = {'sbwy_trip_id': sbwy_trip_id, 'taxi_trip_id': taxi_trip_id,\
@@ -96,6 +145,7 @@ df_sbwy_individual_trip['date_time'] = pd.to_datetime(df_sbwy_individual_trip['d
 
 # read group matches
 df_sbwy_taxi_matching = pd.read_csv(sbwy_taxi_maching_path)
+df_sbwy_taxi_matching['date_time'] = pd.to_datetime(df_sbwy_taxi_matching['date_time'])
 dict_matching = group_df_rows(df_sbwy_taxi_matching, 'match_id')
 
 # read format taxisharing trips
@@ -134,6 +184,7 @@ dict_good_taxisharing = df_good_taxisharing.T.to_dict()
 for key, taxisharing in dict_good_taxisharing.iteritems():
     print taxisharing['taxi_sampn_perno_tripno'], taxisharing['sbwy_sampn_perno_tripno'], taxisharing['destination_destination_distance'],\
     taxisharing['taxi_sbwy_integration_distance'], taxisharing['sbwy_destination_first'], taxisharing['integration_destination_distance']
+
 ## plot saving and wasting
 # list_time_saving = remove_outliers(list_time_saving)
 # list_time_wasting = remove_outliers(list_time_wasting)
