03f37b00debc060c8ee08dbea347a48e7321bcbd
Atila Jr <amsj.master@gmail.com>
2018-03-08 15:56:00 -0300

    remove 99

--- src/summarization/data_survey_summarization.py
+++ src/summarization/data_survey_summarization.py
@@ -59,6 +59,11 @@ def total_departure_arrival_trips(df_trips, chart_name):
 	df_trips_bus = df_trips[df_trips['MODE_G8'] == 3]
 	df_trips_taxi = df_trips[df_trips['MODE_G8'] == 5]
 
+	df_trips_subway = df_trips_subway[df_trips_subway['HR_DEP'] != 99]
+	df_trips_subway_bus = df_trips_subway[df_trips_subway_bus['HR_DEP'] != 99]
+	df_trips_bus = df_trips_subway[df_trips_bus['HR_DEP'] != 99]
+	df_trips_taxi = df_trips_subway[df_trips_taxi['HR_DEP'] != 99]
+
 	ecdf_subway = ecdf_df(df_trips_subway, 'HR_DEP')
 	ecdf_subway_bus = ecdf_df(df_trips_subway_bus, 'HR_DEP')
 	ecdf_bus = ecdf_df(df_trips_bus, 'HR_DEP')
