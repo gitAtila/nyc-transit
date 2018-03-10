c5a26f1d5ed64dc615582651092a4cd0e7c1bf6d
Atila Jr <amsj.master@gmail.com>
2018-03-10 18:44:17 -0300

    matplotlib

--- src/analysis/max_benefit_time_cost.py
+++ src/analysis/max_benefit_time_cost.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
+import matplotlib
 matplotlib.use('Agg')
 from statsmodels.distributions.empirical_distribution import ECDF
 
