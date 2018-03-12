87c3f62e83403fa8fcbaee5ab80b4492db8ad253
Atila Jr <amsj.master@gmail.com>
2018-03-11 22:44:20 -0300

    tick label

--- src/analysis/cost_policies.py
+++ src/analysis/cost_policies.py
@@ -46,7 +46,8 @@ dict_counts = distinct_taxi_transit_pair(cost_1_1_1_path, dict_counts)
 
 df_counts = pd.DataFrame(dict_counts, )
 df_counts = df_counts[['taxi', 'transit', 'pair']]
-ax = df_counts.plot(xticks=['0.5 0.5 0.5', '1 0.5 0.5', '0.5 1 0.5', '0.5 0.5 1',\
-'1 1 0.5', '1 0.5 1', '0.5 1 1', '1 1 1'] ,kind='bar')
+ax = df_counts.plot(kind='bar')
+ax.xaxis.set_ticklabels(['0.5 0.5 0.5', '1 0.5 0.5', '0.5 1 0.5', '0.5 0.5 1',\
+'1 1 0.5', '1 0.5 1', '0.5 1 1', '1 1 1'])
 fig = ax.get_figure()
 fig.savefig(result_path, bbox_inches='tight')
