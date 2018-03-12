292ee8b453765728486b4aab29a0bb41bb163cbb
Atila Jr <amsj.master@gmail.com>
2018-03-11 22:25:56 -0300

    xticks

--- src/analysis/cost_policies.py
+++ src/analysis/cost_policies.py
@@ -44,7 +44,9 @@ dict_counts = distinct_taxi_transit_pair(cost_1_05_1_path, dict_counts)
 dict_counts = distinct_taxi_transit_pair(cost_05_1_1_path, dict_counts)
 dict_counts = distinct_taxi_transit_pair(cost_1_1_1_path, dict_counts)
 
-df_counts = pd.DataFrame(dict_counts)
-ax = df_counts.plot(kind='bar')
+df_counts = pd.DataFrame(dict_counts, )
+df_counts = df_counts[['taxi', 'transit', 'pair']]
+ax = df_counts.plot(xticks=['0.5 0.5 0.5', '1 0.5 0.5', '0.5 1 0.5', '0.5 0.5 1',\
+'1 1 0.5', '0.5 1 1', '1 1 1'] ,kind='bar')
 fig = ax.get_figure()
 fig.savefig(result_path, bbox_inches='tight')
