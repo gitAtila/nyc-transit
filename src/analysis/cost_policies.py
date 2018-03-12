0fe8fa0f5155530bc70ff2bf6a3ec54481503aae
Atila Jr <amsj.master@gmail.com>
2018-03-11 22:05:27 -0300

    cost policies

---
+++ src/analysis/cost_policies.py
@@ -0,0 +1,50 @@
+'''
+    Evaluate pricing policies
+'''
+
+from sys import argv
+import pandas as pd
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+from statsmodels.distributions.empirical_distribution import ECDF
+
+cost_05_05_05_path = argv[1]
+cost_1_05_05_path = argv[2]
+cost_05_1_05_path = argv[3]
+cost_05_05_1_path = argv[4]
+cost_1_1_05_path = argv[5]
+cost_1_05_1_path = argv[6]
+cost_05_1_1_path = argv[7]
+cost_1_1_1_path = argv[8]
+#
+result_path[9]
+
+def distinct_taxi_transit_pair(cost_path, dict_counts):
+    df_cost = pd.read_csv(cost_path)
+    dict_counts['taxi'].append(len(df_cost['taxi_id'].unique()))
+    dict_counts['transit'].append(len(df_cost['transit_id'].unique()))
+    dict_counts['pair'].append(len(df_cost.groupby(['taxi_id','transit_id']).count()))
+    return dict_counts
+
+dict_counts = dict()
+dict_counts['taxi'] = []
+dict_counts['transit'] = []
+dict_counts['pair'] = []
+
+dict_counts = distinct_taxi_transit_pair(cost_05_05_05_path, dict_counts)
+dict_counts = distinct_taxi_transit_pair(cost_1_05_05_path, dict_counts)
+dict_counts = distinct_taxi_transit_pair(cost_05_1_05_path, dict_counts)
+dict_counts = distinct_taxi_transit_pair(cost_05_05_1_path, dict_counts)
+dict_counts = distinct_taxi_transit_pair(cost_1_1_05_path, dict_counts)
+dict_counts = distinct_taxi_transit_pair(cost_1_05_1_path, dict_counts)
+dict_counts = distinct_taxi_transit_pair(cost_05_1_1_path, dict_counts)
+dict_counts = distinct_taxi_transit_pair(cost_1_1_1_path, dict_counts)
+
+df_counts = pd.DataFrame(dict_counts)
+ax = df_counts.plot(kind=bar)
+fig = ax.get_figure()
+fig.savefig(result_path, bbox_inches='tight')
