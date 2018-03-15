f3f1d995f5a67eeffba55ade920fd641f124b9b3
Atila Jr <amsj.master@gmail.com>
2018-03-15 10:52:34 -0300

    fixing bugs

--- src/analysis/cost_policies.py
+++ src/analysis/cost_policies.py
@@ -45,6 +45,7 @@ dict_counts = distinct_taxi_transit_pair(cost_1_1_05_path, dict_counts)
 dict_counts = distinct_taxi_transit_pair(cost_1_1_1_path, dict_counts)
 
 df_counts = pd.DataFrame(dict_counts)
+print df_counts
 df_counts = df_counts[['taxi', 'transit', 'pair']]
 df_counts.rename(columns={'taxi': 'Particular', 'transit': 'Coletivo', 'pair': 'Par'},\
 inplace=True)
