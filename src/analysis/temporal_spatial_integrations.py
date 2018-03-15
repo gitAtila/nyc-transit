326bc68e3f7a54f42e46f343e055e9e56e3fb4e4
Atila Jr <amsj.master@gmail.com>
2018-03-15 08:46:14 -0300

    save charts as pdf

--- src/analysis/temporal_spatial_integrations.py
+++ src/analysis/temporal_spatial_integrations.py
@@ -44,7 +44,7 @@ list_transit_stop_options = df_temporal_spatial.groupby('transit_id')['stop_id']
 list_taxi_position_options = df_temporal_spatial.groupby('taxi_id')['taxi_pos_sequence'].nunique().tolist()
 
 plot_cdf_two_curves(list_taxi_per_transit, list_transit_per_taxi, 'Transp. Particular por Transp. Coletivo',\
-'Transp. Coletivo por Transp. Particular', '# de Opcoes de Integracao', result_path + 'cdf_opcoes_integracao_viagens.png')
+'Transp. Coletivo por Transp. Particular', '# de Opcoes de Integracao', result_path + 'cdf_opcoes_integracao_viagens.pdf')
 
 plot_cdf_two_curves(list_transit_stop_options, list_taxi_position_options, 'Paradas de Transp. Coletivo',\
-'Posicoes de Transp. Particular', '# de Opcoes de Integracao', result_path + 'cdf_opcoes_integracao_posicoes.png')
+'Posicoes de Transp. Particular', '# de Opcoes de Integracao', result_path + 'cdf_opcoes_integracao_posicoes.pdf')
