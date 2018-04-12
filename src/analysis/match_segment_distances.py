279e6daef25335c146d8173a485d6055c81d1e38
Atila Jr <amsj.master@gmail.com>
2018-04-12 11:55:38 -0300

    merge destinations distance

--- src/analysis/match_segment_distances.py
+++ src/analysis/match_segment_distances.py
@@ -18,35 +18,40 @@ df_matches['taxi_destination_time'] = pd.to_datetime(df_matches['taxi_destinatio
 # get distances
 list_integration_distance = []
 list_shared_distance = []
-list_taxi_private = []
-list_transit_private = []
+# list_taxi_private = []
+# list_transit_private = []
+list_destinations_distance = []
 for index, match in df_matches.iterrows():
     total_distance = match['integration_distance'] + match['shared_distance'] + match['destinations_distance']
     list_integration_distance.append(match['integration_distance']/total_distance)
     list_shared_distance.append(match['shared_distance']/total_distance)
-    if match['transit_destination_time'] > match['taxi_destination_time']:
-        list_transit_private.append(match['destinations_distance']/total_distance)
-        list_taxi_private.append(0)
-    else:
-        list_transit_private.append(0)
-        list_taxi_private.append(match['destinations_distance']/total_distance)
+    list_destinations_distance.append(match['destinations_distance']/total_distance)
+    # if match['transit_destination_time'] > match['taxi_destination_time']:
+    #     list_transit_private.append(match['destinations_distance']/total_distance)
+    #     list_taxi_private.append(0)
+    # else:
+    #     list_transit_private.append(0)
+    #     list_taxi_private.append(match['destinations_distance']/total_distance)
 
 # plot
 list_integration_distance.sort()
 list_shared_distance.sort()
-list_taxi_private.sort()
-list_transit_private.sort()
+# list_taxi_private.sort()
+# list_transit_private.sort()
+list_destinations_distance.sort()
 
 ecdf_integration_distance = ECDF(list_integration_distance)
 ecdf_shared_distance = ECDF(list_shared_distance)
-ecdf_taxi_private = ECDF(list_taxi_private)
-ecdf_transit_private = ECDF(list_transit_private)
+# ecdf_taxi_private = ECDF(list_taxi_private)
+# ecdf_transit_private = ECDF(list_transit_private)
+ecdf_destinations_distance = ECDF(list_destinations_distance)
 
 fig, ax = plt.subplots()
 plt.plot(ecdf_integration_distance.x, ecdf_integration_distance.y, label='integration distance')
 plt.plot(ecdf_shared_distance.x, ecdf_shared_distance.y, label='shared distance')
-plt.plot(ecdf_taxi_private.x, ecdf_taxi_private.y, label='taxi private')
-plt.plot(ecdf_transit_private.x, ecdf_transit_private.y, label='transit private')
+# plt.plot(ecdf_taxi_private.x, ecdf_taxi_private.y, label='taxi private')
+# plt.plot(ecdf_transit_private.x, ecdf_transit_private.y, label='transit private')
+plt.plot(ecdf_destinations_distance.x, ecdf_destinations_distance.y, label='destinations distance')
 
 # ax.xaxis.set_major_locator(ticker.MultipleLocator(20)) # set x sticks interal
 plt.grid()
