5eccb4ddeafc41af9aa5f6033780ab3c8a9d0955
Atila Jr <amsj.master@gmail.com>
2018-04-17 10:20:42 -0300

    transit origin integration

--- src/analysis/match_segment_durations.py
+++ src/analysis/match_segment_durations.py
@@ -22,9 +22,9 @@ def group_df_rows(df, key_label):
 # read and transform
 df_private = pd.read_csv(private_trips)
 df_private['date_time'] = pd.to_datetime(df_private['date_time'])
-df_private_origins = df_private.loc[df_private.groupby('sampn_perno_tripno')['date_time'].idxmin()]
+# df_private_origins = df_private.loc[df_private.groupby('sampn_perno_tripno')['date_time'].idxmin()]
 
-print df_private_origins
+print df_private
 # dict_private_trip = group_df_rows(df_private, 'sampn_perno_tripno')
 # for key, list_positions in dict_private_trip.iteritems():
 #     new_list_positions = [list_positions[0], list_positions[-1]]
@@ -43,13 +43,19 @@ list_integration_duration = []
 list_shared_duration = []
 list_destinations_distance = []
 for index, match in df_matches.iterrows():
-    transit_origin_datetime = df_private_origins[df_private_origins['sampn_perno_tripno'] == match['transit_id']]['date_time']
-    taxi_origin_datetime = df_private_origins[df_private_origins['sampn_perno_tripno'] == match['taxi_id']]['date_time']
+    df_transit_private_trip = df_private[df_private['sampn_perno_tripno']== match['transit_id']]
+    transit_origin_datetime = df_transit_private_trip.loc[df_transit_private_trip['date_time'].idxmin()]['date_time'].iloc[0]
+    transit_integration_datetime = df_transit_private_trip[df_transit_private_trip['stop_id'] == match['stop_id']]['date_time'].iloc[0]
+
+    df_taxi_private_trip = df_private[df_private['sampn_perno_tripno'] == match['taxi_id']]
+    taxi_origin_datetime = df_taxi_private_trip.loc[df_taxi_private_trip['date_time'].idxmin()]['date_time'].iloc[0]
 
     print 'transit_origin_datetime', transit_origin_datetime
-    print 'taxi_origin_datetime', taxi_origin_datetime
+    print 'transit_integration_datetime', transit_integration_datetime
 
-    
+    print '\ntaxi_origin_datetime', taxi_origin_datetime
+
+
 
 #     total_distance = match['integration_distance'] + match['shared_distance'] + match['destinations_distance']
 #     list_integration_distance.append(match['integration_distance']/total_distance)
