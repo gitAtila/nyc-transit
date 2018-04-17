09a5c57230c8492a9fb41a3b1e56db5f92891539
Atila Jr <amsj.master@gmail.com>
2018-04-17 09:54:03 -0300

    print origins

--- src/analysis/match_segment_durations.py
+++ src/analysis/match_segment_durations.py
@@ -22,7 +22,9 @@ def group_df_rows(df, key_label):
 # read and transform
 df_private = pd.read_csv(private_trips)
 df_private['date_time'] = pd.to_datetime(df_private['date_time'])
-print df_private
+df_private_origins = df_private.loc[df_private.groupby('sampn_perno_tripno')['date_time'].idxmin()]
+
+print df_private_origins
 # dict_private_trip = group_df_rows(df_private, 'sampn_perno_tripno')
 # for key, list_positions in dict_private_trip.iteritems():
 #     new_list_positions = [list_positions[0], list_positions[-1]]
@@ -34,33 +36,43 @@ print df_matches
 df_matches['transit_destination_time'] = pd.to_datetime(df_matches['transit_destination_time'])
 df_matches['taxi_destination_time'] = pd.to_datetime(df_matches['taxi_destination_time'])
 
-# get distances
-list_integration_distance = []
-list_shared_distance = []
+# get durations
+list_transit_private_duration = []
+list_taxi_private_duration = []
+list_integration_duration = []
+list_shared_duration = []
 list_destinations_distance = []
 for index, match in df_matches.iterrows():
-    total_distance = match['integration_distance'] + match['shared_distance'] + match['destinations_distance']
-    list_integration_distance.append(match['integration_distance']/total_distance)
-    list_shared_distance.append(match['shared_distance']/total_distance)
-    list_destinations_distance.append(match['destinations_distance']/total_distance)
+    transit_origin_datetime = df_private_origins[df_private_origins['sampn_perno_tripno'] == match['transit_id']]['date_time']
+    taxi_origin_datetime = df_private_origins[df_private_origins['sampn_perno_tripno'] == match['taxi_id']]['date_time']
 
-# plot
-list_integration_distance.sort()
-list_shared_distance.sort()
-list_destinations_distance.sort()
+    print 'transit_origin_datetime', transit_origin_datetime
+    print 'taxi_origin_datetime', taxi_origin_datetime
 
-ecdf_integration_distance = ECDF(list_integration_distance)
-ecdf_shared_distance = ECDF(list_shared_distance)
-ecdf_destinations_distance = ECDF(list_destinations_distance)
+    
 
-fig, ax = plt.subplots()
-plt.plot(ecdf_integration_distance.x, ecdf_integration_distance.y, label='integration duration')
-plt.plot(ecdf_shared_distance.x, ecdf_shared_distance.y, label='shared duration')
-plt.plot(ecdf_destinations_distance.x, ecdf_destinations_distance.y, label='destinations duration')
-
-plt.grid()
-plt.legend()
-ax.set_xlabel('segment duration / total duration')
-ax.set_ylabel('ECDF')
-plt.tight_layout()
-fig.savefig(result_path)
+#     total_distance = match['integration_distance'] + match['shared_distance'] + match['destinations_distance']
+#     list_integration_distance.append(match['integration_distance']/total_distance)
+#     list_shared_distance.append(match['shared_distance']/total_distance)
+#     list_destinations_distance.append(match['destinations_distance']/total_distance)
+#
+# # plot
+# list_integration_distance.sort()
+# list_shared_distance.sort()
+# list_destinations_distance.sort()
+#
+# ecdf_integration_distance = ECDF(list_integration_distance)
+# ecdf_shared_distance = ECDF(list_shared_distance)
+# ecdf_destinations_distance = ECDF(list_destinations_distance)
+#
+# fig, ax = plt.subplots()
+# plt.plot(ecdf_integration_distance.x, ecdf_integration_distance.y, label='integration duration')
+# plt.plot(ecdf_shared_distance.x, ecdf_shared_distance.y, label='shared duration')
+# plt.plot(ecdf_destinations_distance.x, ecdf_destinations_distance.y, label='destinations duration')
+#
+# plt.grid()
+# plt.legend()
+# ax.set_xlabel('segment duration / total duration')
+# ax.set_ylabel('ECDF')
+# plt.tight_layout()
+# fig.savefig(result_path)
