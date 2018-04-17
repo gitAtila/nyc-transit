c47edfecb284fdead6b72e62e347c3ad3ff80200
Atila Jr <amsj.master@gmail.com>
2018-04-17 11:52:58 -0300

    plot transit and taxi relative duration

--- src/analysis/match_segment_durations.py
+++ src/analysis/match_segment_durations.py
@@ -9,7 +9,9 @@ from statsmodels.distributions.empirical_distribution import ECDF
 
 private_trips = argv[1]
 matches_path = argv[2]
-result_path = argv[3]
+transit_result_path = argv[3]
+taxi_result_path = argv[4]
+
 
 def group_df_rows(df, key_label):
     dict_grouped = dict()
@@ -19,17 +21,31 @@ def group_df_rows(df, key_label):
         dict_grouped.setdefault(key, []).append(row.to_dict())
     return dict_grouped
 
+def plot_relative_durations(list_private_durations, list_shared_durations, list_destination_durations, result_path):
+    list_private_durations.sort()
+    list_shared_durations.sort()
+    list_destination_durations.sort()
+
+    ecdf_private_durations = ECDF(list_private_durations)
+    ecdf_shared_durations = ECDF(list_shared_durations)
+    ecdf_destination_durations = ECDF(list_destination_durations)
+
+    fig, ax = plt.subplots()
+    plt.plot(ecdf_private_durations.x, ecdf_private_durations.y, label='private')
+    plt.plot(ecdf_shared_durations.x, ecdf_shared_durations.y, label='shared')
+    plt.plot(ecdf_destination_durations.x, ecdf_destination_durations.y, label='destination')
+
+    plt.grid()
+    plt.legend()
+    ax.set_xlabel('segment duration / total duration')
+    ax.set_ylabel('ECDF')
+    plt.tight_layout()
+    fig.savefig(result_path)
+
 # read and transform
 df_private = pd.read_csv(private_trips)
 df_private['date_time'] = pd.to_datetime(df_private['date_time'])
-# df_private_origins = df_private.loc[df_private.groupby('sampn_perno_tripno')['date_time'].idxmin()]
-
 print df_private
-# dict_private_trip = group_df_rows(df_private, 'sampn_perno_tripno')
-# for key, list_positions in dict_private_trip.iteritems():
-#     new_list_positions = [list_positions[0], list_positions[-1]]
-#     dict_private_trip[key] = new_list_positions
-# print len(dict_private_trip)
 
 df_matches = pd.read_csv(matches_path)
 print df_matches
@@ -44,44 +60,52 @@ list_integration_duration = []
 list_shared_duration = []
 list_destinations_distance = []
 for index, match in df_matches.iterrows():
+    # private time
     df_transit_private_trip = df_private[df_private['sampn_perno_tripno']== match['transit_id']]
     transit_origin_datetime = df_transit_private_trip.loc[df_transit_private_trip['date_time'].idxmin()]['date_time']
     transit_integration_datetime = df_transit_private_trip[df_transit_private_trip['stop_id'] == match['stop_id']]['date_time'].iloc[0]
-    transit_private_duration = (transit_integration_datetime - transit_origin_datetime).total_seconds()/60
+    transit_private_duration = (transit_integration_datetime - transit_origin_datetime).total_seconds()
+    # list_transit_private_duration.append(transit_private_duration/60)
 
     df_taxi_private_trip = df_private[df_private['sampn_perno_tripno'] == match['taxi_id']]
     taxi_origin_datetime = df_taxi_private_trip.loc[df_taxi_private_trip['date_time'].idxmin()]['date_time']
-    taxi_private_duration = (match['taxi_arrival_time_transit_stop'] - taxi_origin_datetime).total_seconds()/60
+    taxi_private_duration = (match['taxi_arrival_time_transit_stop'] - taxi_origin_datetime).total_seconds()
+    # list_taxi_private_duration.append(taxi_private_duration/60)
 
-    print 'transit_private_duration', transit_private_duration
-    print 'taxi_private_duration', taxi_private_duration
+    # waiting time
+    if transit_integration_datetime < match['taxi_arrival_time_transit_stop']:
+        transit_private_duration += (match['taxi_arrival_time_transit_stop'] - transit_integration_datetime).total_seconds()
+        shared_origin_time = match['taxi_arrival_time_transit_stop']
+    else
+        taxi_private_duration += (transit_integration_datetime - match['taxi_arrival_time_transit_stop']).total_seconds()
+        shared_origin_time = transit_integration_datetime
 
-    # print '\ntaxi_origin_datetime', taxi_origin_datetime
+    # shared and destination durations
+    if match['taxi_destination_time'] < match['transit_destination_time']:
+        shared_duration = (match['taxi_destination_time'] - shared_origin_time).total_seconds()
+        taxi_destination_duration = 0
+        transit_destination_duration = (match['transit_destination_time'] - match['taxi_destination_time']).total_seconds()
 
+    elif match['taxi_destination_time'] > match['transit_destination_time']:
+        shared_duration = (match['transit_destination_time'] - shared_origin_time).total_seconds()
+        taxi_destination_duration = (match['taxi_destination_time'] - match['transit_destination_time']).total_seconds()
+        transit_destination_duration = 0
+    else:
+        shared_duration = (match['transit_destination_time'] - shared_origin_time).total_seconds()
+        taxi_destination_duration = 0
+        transit_destination_duration = 0
 
+    total_transit_duration = (match['transit_destination_time'] - transit_origin_datetime).totoal_seconds()
+    total_taxi_duration = (match['taxi_destination_time'] - taxi_origin_datetime).total_seconds()
 
-#     total_distance = match['integration_distance'] + match['shared_distance'] + match['destinations_distance']
-#     list_integration_distance.append(match['integration_distance']/total_distance)
-#     list_shared_distance.append(match['shared_distance']/total_distance)
-#     list_destinations_distance.append(match['destinations_distance']/total_distance)
-#
-# # plot
-# list_integration_distance.sort()
-# list_shared_distance.sort()
-# list_destinations_distance.sort()
-#
-# ecdf_integration_distance = ECDF(list_integration_distance)
-# ecdf_shared_distance = ECDF(list_shared_distance)
-# ecdf_destinations_distance = ECDF(list_destinations_distance)
-#
-# fig, ax = plt.subplots()
-# plt.plot(ecdf_integration_distance.x, ecdf_integration_distance.y, label='integration duration')
-# plt.plot(ecdf_shared_distance.x, ecdf_shared_distance.y, label='shared duration')
-# plt.plot(ecdf_destinations_distance.x, ecdf_destinations_distance.y, label='destinations duration')
-#
-# plt.grid()
-# plt.legend()
-# ax.set_xlabel('segment duration / total duration')
-# ax.set_ylabel('ECDF')
-# plt.tight_layout()
-# fig.savefig(result_path)
+    # compute and add to response relative durations
+    list_transit_private_duration.append(float(transit_private_duration)/float(total_transit_duration))
+    list_transit_shared_duration.append(float(shared_duration)/float(total_transit_duration))
+    list_transit_destination_duration.append(float(transit_destination_duration)/float(total_transit_duration))
+
+    list_taxi_private_duration.append(float(taxi_private_duration)/float(total_taxi_duration))
+    list_taxi_shared_duration.append(float(shared_duration)/float(total_taxi_duration))
+    list_taxi_destination_duration.append(float(taxi_destination_duration)/float(total_taxi_duration))
+
+plot_relative_durations(list_transit_private_duration, list_transit_shared_duration, list_transit_destination_duration, transit_result_path)
+plot_relative_durations(list_taxi_private_duration, list_taxi_shared_duration, list_taxi_destination_duration, taxi_result_path)
