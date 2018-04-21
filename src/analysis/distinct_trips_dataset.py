60f6204af9b92882b7d10cfd41d13eb4e5072c89
Atila Jr <amsj.master@gmail.com>
2018-04-21 11:47:08 -0300

    line plot

--- src/analysis/distinct_trips_dataset.py
+++ src/analysis/distinct_trips_dataset.py
@@ -59,7 +59,7 @@ dict_counts = distinct_taxi_transit_pair(dict_counts, temporal_spatial_20min_10x
 df_counts = pd.DataFrame(dict_counts, index=['real', '5x_10min', '5x_20min', '10x_10min', '10x_20min'])
 df_counts = df_counts[['temporal_spatial', 'cost', 'max_benefit']]
 print df_counts
-ax = df_counts.plot(kind='bar')
+ax = df_counts.plot(kind='line')
 # ax = df_counts.plot()
 # ax.xaxis.set_major_locator(ticker.MultipleLocator(1)) # set x sticks interal
 # ax.xaxis.set_ticklabels(['real', '5x_10min', '5x_20min', '10x_10min', '10x_20min'])
