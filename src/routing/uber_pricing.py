aaaa4686b9f23d8d4843d127dcc1b2f63e574ff3
Atila Jr <amsj.master@gmail.com>
2018-04-22 19:02:07 -0300

    trip id

--- src/routing/uber_pricing.py
+++ src/routing/uber_pricing.py
@@ -34,7 +34,7 @@ df_all_trips = pd.read_csv(private_trips_path)
 df_taxi_trips = df_all_trips[df_all_trips['MODE_G10'] == 7]
 
 for index, trip in df_taxi_trips.iterrows():
-    trip_id = trip['sampn'] + '_' + trip['perno'] + '_' + trip['tripno']
+    trip_id = str(trip['sampn']) + '_' + str(trip['perno']) + '_' + str(trip['tripno'])
 
     estimatives = uber_prices(token, trip['lat_origin'], trip['lon_origin'],\
     trip['lat_destination'], trip['lon_destination'])
