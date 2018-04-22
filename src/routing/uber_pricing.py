bb48218cbb643fd64d818eb5c50fb230771b3173
Atila Jr <amsj.master@gmail.com>
2018-04-22 18:53:16 -0300

    estimate uber pricing

---
+++ src/routing/uber_pricing.py
@@ -0,0 +1,49 @@
+'''
+    Compute uber pricing given origin and destination positions
+'''
+from sys import argv
+import panas as pd
+
+from uber_rides.session import Session
+from uber_rides.client import UberRidesClient
+
+import requests
+import json
+
+# start_latitude = 37.7752315
+# start_longitude = -122.418075
+# end_latitude = 37.7752415
+# end_longitude = -122.518075
+token = argv[1]
+private_trips_path = argv[2]
+
+def uber_prices(token, start_latitude, start_longitude, end_latitude, end_longitude):
+
+    session = Session(server_token=token)
+    client = UberRidesClient(session)
+
+    response = client.get_price_estimates(
+        start_latitude=start_latitude,
+        start_longitude=start_longitude,
+        end_latitude=end_latitude,
+        end_longitude=end_longitude,
+        seat_count=2
+    )
+
+    estimate = response.json.get('prices')
+    print estimate
+    return estimate
+
+df_all_trips = pd.read_csv(private_trips_path)
+df_taxi_trips = df_all_trips[df_all_trips['MODE_G10'] == 7]
+
+for index, trip in df_taxi_trips.iterrows():
+    trip_id = trip['sampn_perno_tripno']
+
+    estimatives = uber_prices(token, trip['lat_origin'], trip['lon_origin'],\
+    trip['lat_destination'], trip['lon_destination'])
+
+    print trip_id
+    print estimatives
+
+    break
