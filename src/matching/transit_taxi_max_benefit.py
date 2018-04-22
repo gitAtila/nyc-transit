bd6b23687841c78ab2800c1d3d71228bef925702
Atila Jr <amsj.master@gmail.com>
2018-04-22 16:38:48 -0300

    fixing

--- src/matching/transit_taxi_max_benefit.py
+++ src/matching/transit_taxi_max_benefit.py
@@ -49,8 +49,7 @@ def best_integration_possibility(df_matches, df_transit_private_trip, transit_fa
 
             # combine utilities
             integration_utility = (transit_saving_time * transit_factor) + (taxi_saving_money * (1-transit_factor))
-            # integration_utility2 = transit_saving_time * taxi_saving_money
-            # print integration_utility, integration_utility2
+            # integration_utilimlity, integration_utility2
 
             # get the maximum utility
             if integration_utility > maximum_utility:
