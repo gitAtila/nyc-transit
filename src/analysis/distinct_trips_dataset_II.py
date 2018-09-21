2d1bc131c043d6a4fb160a46805b59155f1d031c
Atila Jr <amsj.master@gmail.com>
2018-09-21 06:33:42 -0300

    labels

--- src/analysis/distinct_trips_dataset_II.py
+++ src/analysis/distinct_trips_dataset_II.py
@@ -55,6 +55,6 @@ print df_counts
 ax = df_counts.plot(kind='line')
 # ax = df_counts.plot()
 # ax.xaxis.set_major_locator(ticker.MultipleLocator(1)) # set x sticks interal
-# ax.xaxis.set_ticklabels(['real', '5x_10min', '5x_20min', '10x_10min', '10x_20min'])
+ax.xaxis.set_ticklabels(['real', '5x_10min', '5x_20min', '10x_10min', '10x_20min'])
 fig = ax.get_figure()
 fig.savefig(result_path, bbox_inches='tight')
